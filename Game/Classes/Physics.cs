d726d9d0377c7e65c13983b80cf18bba90d55176
Илья Шилов <stud0000225660@study.utmn.ru>
2022-05-14 23:32:24 +0500

    Созданы прототип игры(сформированы классы,прописана физика,генерация платформ)

---
+++ Game/Classes/Physics.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.Classes
+{
+    public class Physics
+    {
+        public Modify mod;
+        float gravity;
+        float a;
+
+        public float dx;
+        
+
+        public Physics(PointF position, Size size)
+        {
+            mod = new Modify(position, size);
+            gravity = 0;
+            a = 0.4f;
+            dx = 0;
+        }
+
+        public void CalculatePhysics()
+        {
+            if (dx != 0)
+            {
+                mod.position.X += dx;
+            }
+            if(mod.position.Y < 700)
+            {
+                mod.position.Y += gravity;
+                gravity += a;
+                Touch();
+            }
+        }
+
+        public void Touch()
+        {
+            for(int i = 0; i < PlatformGenerate.platforms.Count; i++)
+            {
+                var platform = PlatformGenerate.platforms[i];
+                if(mod.position.X + mod.size.Width/2 >= platform.mod.position.X && 
+                    mod.position.X + mod.size.Width/2 <= platform.mod.position.X + platform.mod.size.Width)
+                {
+                    if(mod.position.Y + mod.size.Height >= platform.mod.position.Y &&
+                        mod.position.Y + mod.size.Height <= platform.mod.position.Y + platform.mod.size.Height)
+                    {
+                        if (gravity > 0)
+                        {
+                            gravity = -8; //сила прыжка
+                        }
+                    }
+                }
+            }
+        }
+            
+    }
+}
+
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -1,20 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Game.Classes;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Collections.Generic;
 namespace Game
 {
     public partial class Form1 : Form
     {
+        Player player;
+        Timer timer;
         public Form1()
         {
             InitializeComponent();
+            Init();
+            timer = new Timer();
+            timer.Interval = 15;
+            timer.Tick += new EventHandler(Update);
+            timer.Start();
+            this.KeyDown += new KeyEventHandler(OnKeyboardPressed);
+            this.KeyUp += new KeyEventHandler(OnKeyboardUp);
+            this.BackgroundImage = Properties.Resources.back;
+            this.Paint += new PaintEventHandler(OnRepaint);
+        }
+
+        public void Init()
+        {
+            PlatformGenerate.platforms = new List<Platform>();
+            PlatformGenerate.AddPlatform(new PointF(100, 400));
+            PlatformGenerate.startPos = 400;
+            PlatformGenerate.GenerateStart();
+            player = new Player();
+        }
+
+        private void OnKeyboardUp(object sender, KeyEventArgs e)
+        {
+            player.imagePlayer = Properties.Resources.player;
+            player.physics.dx = 0;
+        }
+
+        private void OnKeyboardPressed(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    player.imagePlayer = Properties.Resources.rightJump;
+                    player.physics.dx = 6;
+                    break;
+                case "Left":
+                    player.imagePlayer = Properties.Resources.leftJump;
+                    player.physics.dx = -6;
+                    break;
+            }
+        }
+
+
+        private void Update(object sender, EventArgs e)
+        {
+            if (player.physics.mod.position.Y >= PlatformGenerate.platforms[0].mod.position.Y + 200)
+                Init();
+            player.physics.CalculatePhysics();
+
+            FollowMode();
+            Invalidate();
+        }
+
+        private void OnRepaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (PlatformGenerate.platforms.Count > 0)
+            {
+                for (int i = 0; i < PlatformGenerate.platforms.Count; i++)
+                    PlatformGenerate.platforms[i].DrawSprite(g);
+            }
+            player.DrawSprite(g);
+        }
+        public void FollowMode()
+        {
+            int offset = 400 - (int)player.physics.mod.position.Y;
+            player.physics.mod.position.Y += offset;
+            for (int i = 0; i < PlatformGenerate.platforms.Count; i++)
+            {
+                var platform = PlatformGenerate.platforms[i];
+                platform.mod.position.Y += offset;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
